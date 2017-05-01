4e24f527d477fc0d0e1ebee758993e50a5ca2bd8
Kevin Cotrone <kevincotrone@gmail.com>
2017-04-30 21:38:57 -0500

    Move to public repo

---
+++ src/Network/Join.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Network.Join (
+    APIKey(..)
+  , DeviceId(..)
+  , SMS(..)
+  , sendSMS
+  , sendPush
+  ) where
+
+import Data.Aeson
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.String
+import Network.URL
+import Network.Wreq
+import Data.ByteString.Lazy (ByteString)
+import Control.Lens
+
+-- | API Key for the account
+newtype APIKey = APIKey {
+  unAPIKey :: Text
+} deriving (Eq, Ord, Show, IsString)
+
+-- | Device ID to send the message to/through
+newtype DeviceId = DeviceId {
+  unDeviceId :: Text
+} deriving (Eq, Ord, Show, IsString)
+
+-- | SMS Message with phone number to send the
+-- message to
+data SMS = SMS {
+  smsNumber :: Text
+, smsText :: Text
+} deriving (Eq, Ord, Show)
+
+joinUrlType :: URLType
+joinUrlType =
+  Absolute $
+    Host (HTTP True) "joinjoaomgcd.appspot.com" Nothing
+
+joinPath :: String
+joinPath = "_ah/api/messaging/v1/sendPush"
+
+data JoinResponse = JoinResponse {
+  joinSuccess :: Bool
+, joinUserAuthError :: Bool
+, joinKind :: Text
+, joinETag :: Text
+}
+
+instance FromJSON JoinResponse where
+  parseJSON (Object o) =
+    JoinResponse
+      <$> (o .: "success")
+      <*> (o .: "userAuthError")
+      <*> (o .: "kind")
+      <*> (o .: "etag")
+
+sendSMS :: APIKey -> DeviceId -> SMS -> IO JoinResponse
+sendSMS (APIKey key) (DeviceId i) (SMS number msg) = do
+  resp <- asJSON =<< (get $ exportURL smsUrl)
+  return $ resp ^. responseBody
+  where
+    smsUrl = URL joinUrlType joinPath params
+    params = (fmap T.unpack) <$> [
+        ("deviceId", i)
+      , ("apikey", key)
+      , ("smsnumber", number)
+      , ("smstext", msg)
+      ]
+
+sendPush :: APIKey -> DeviceId -> Text -> IO JoinResponse
+sendPush (APIKey key) (DeviceId i) msg = do
+  resp <- asJSON =<< (get $ exportURL smsUrl)
+  return $ resp ^. responseBody
+  where
+    smsUrl = URL joinUrlType joinPath params
+    params = (fmap T.unpack) <$> [
+        ("deviceId", i)
+      , ("apikey", key)
+      , ("text", msg)
+      ]
